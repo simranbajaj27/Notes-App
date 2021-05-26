3f890e029a214880398fe327929713508f557e98
Simran Bajaj <simranbajaj27081999@gmail.com>
2021-05-26 20:02:09 +0530

    Added title

--- js/javascript.js
+++ js/javascript.js
@@ -3,41 +3,65 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 
 addBtn.addEventListener('click', function(e) {
+    let titleTxt=document.getElementById("titleTxt");
     let addTxt = document.getElementById("text");
+    let titles=localStorage.getItem("titles");
     let notes=localStorage.getItem("notes");
     if (notes == null) {
         notesObj=[];
+        titleObj=[];
     }
     else {
         notesObj=JSON.parse(notes);
+        titleObj=JSON.parse(titles);
     }
     notesObj.push(addTxt.value);
-    localStorage.setItem("notes",JSON.stringify(notesObj))
+    titleObj.push(titleTxt.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("titles",JSON.stringify(titleObj));
     addTxt.value="";
+    titleTxt.value="";
     //console.log(notesObj);
     showNotes();
 })
 
 function showNotes(){
     let notes=localStorage.getItem("notes");
+    let titles=localStorage.getItem("titles");
     if(notes==null){
         notesObj=[];
+        titleObj=[];
     }
     else{
         notesObj=JSON.parse(notes);
+        titleObj=JSON.parse(titles);
     }
     let html="";
+    let len=notesObj.length;
+    for(let i=0;i<len;i++){
+        html+=`
+        <div class="noteCard card my-2 mx-2" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${titleObj[i]} </h5>
+            <p class="card-text">${notesObj[i]}</p>
+            <a id="${i}" onclick="deleteNotes(this.id)" class="btn btn-primary">Delete</a>
+        </div>
+    </div>
+        `
+    }
+    /*
     notesObj.forEach(function(element,index){
         html+=`
         <div class="noteCard card my-2 mx-2" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Note ${index+1} </h5>
+            <h5 class="card-title">${index+1} </h5>
             <p class="card-text">${element}</p>
             <a id="${index}" onclick="deleteNotes(this.id)" class="btn btn-primary">Delete</a>
         </div>
     </div>
         `
     });
+    */
     let notesElem=document.getElementById('notes');
     if(notesObj.length!=0){
         notesElem.innerHTML=html;
@@ -50,14 +74,19 @@ function showNotes(){
 
 function deleteNotes(index){
     let notes=localStorage.getItem("notes");
+    let titles=localStorage.getItem("titles");
     if(notes==null){
         notesObj=[];
+        titleObj=[];
     }
     else{
         notesObj=JSON.parse(notes);
+        titleObj=JSON.parse(titles);
     }
     notesObj.splice(index,1);
+    titleObj.splice(index,1);
     localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("titles",JSON.stringify(titleObj));
     showNotes();
 }
 
